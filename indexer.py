a40cc6fd4c6364fc6474993f8763b132fe91f744
Hanshal Dabbiru <hanshal.dabbiru@outlook.com>
2025-11-08 16:37:07 -0800

    small changes

--- indexer.py
+++ indexer.py
@@ -1,13 +1,20 @@
 import json 
 import tokenizer
+import zipfile
+from bs4 import BeautifulSoup
 import re
 from nltk.stem import PorterStemmer
+from posting import Posting
 
 
 """
 Add functions here to 1) strip html tags to get raw text and 2) extract the texts from 
 important tags like h2, strong tags.
 """
+
+def extract_text(html):
+    soup = BeautifulSoup(html, "lxml")
+    return soup.get_text(separator=" ", strip=True)
 
 def indexer(corpus):
     """
@@ -24,35 +31,48 @@ def indexer(corpus):
         concurrently, while going through the html file, regex <h{n}>
 
     """
-    for file in corpus:
+    with zipfile.ZipFile(corpus, "r") as zip_ref:
+        for file in zip_ref.namelist():
+            try:
+                # 1) load json
+                with zip_ref.open(file) as f:
+                    data = json.load(f)
+                print("success: ", file)
+                html = data["content"]
+                encoding = data["encoding"]
+                url = data["url"].split("#")[0]
 
-        # 1) load json
-        with open(file, "r", encoding="utf-8") as f:
-            data = json.load(f)
+                # 2) text extraction + stemming
+                plain = extract_text(html)
+                tokens = tokenizer.tokenize(plain)
+                stemmed_tokens = [stemmer.stem(token) for token in tokens]
 
-        html = data["content"]
-        url = data["url"].split("#")[0]
+                # 3) important words
+                important_tokens = []
+                # implement imp words logic here
+                important_stemmed = [stemmer.stem(token) for token in important_tokens]
 
-        # 2) text extraction + stemming
-        plain = ""
-        # plain = strip_html(html)
-        tokens = tokenizer.tokenize(plain)
-        stemmed_tokens = [stemmer.stem(token) for token in tokens]
+                # 4) build index
+                for i, word in enumerate(stemmed_tokens):
+                    if word not in index:
+                        posting = Posting(url, 1)
+                        index[word] = [posting]
+                    if url not in index[word]:
+                        posting = Posting(url, 1)
+                        index[word].append(posting)
+                    else: # url is already associated with the word, so increment the posting's frequency
+                        j = index[word].index(url)
+                        index[word][j].freq += 1
+                        # insert to index, could potentially include tuple with url weight for
+                        # when they are different importance levels
 
-        # 3) important words
-        important_tokens = []
-        # implement imp words logic here
-        important_stemmed = [stemmer.stem(token) for token in important_tokens]
-
-        # 4) build index
-        for i, word in enumerate(stemmed_tokens):
-            if word not in index:
-                index[word] = {}
-            if url not in index[word]:
-                pass
-                # insert to index, could potentially include tuple with url weight for
-                # when they are different importance levels
+            except json.decoder.JSONDecodeError as e:
+                print(e)
+                print("error:", file)
+                
+    print(index)
     return index
 
+
 if __name__ == "__main__":
-    index = indexer(developer)
+    index = indexer("developer.zip")
